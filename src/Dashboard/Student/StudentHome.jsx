7fbf4049922e0e59fd880d3672a167b71aaf7a83
mohammad munna <mohammadmunna4141@gmail.com>
2023-08-07 21:42:33 +0600

    student

--- src/Dashboard/Student/StudentHome.jsx
+++ src/Dashboard/Student/StudentHome.jsx
@@ -5,7 +5,7 @@ const StudentHome = () => {
     const { user } = useContext(AuthContext)
     return (
         <div>
-             <h2 className='text-4xl font-serif text-center mt-10 text-red-800 leading-relaxed'>Welcome to student dashboard<br />{user?.displayName}</h2>
+             <h2 className='text-4xl font-serif text-center mt-10 text-red-800 leading-relaxed'>Welcome to student Dashboard<br />{user?.displayName}</h2>
         </div>
     );
 };
